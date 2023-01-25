7a308899c9e6d93dea853f46782026ae53c064fe
Ifiok Jr <ifiokotung@gmail.com>
2023-01-25 04:58:44 +0000

    feat: initial commit

---
+++ devenv.nix
@@ -0,0 +1,54 @@
+{ pkgs, ... }:
+
+{
+  packages = [ 
+    pkgs.cargo-all-features
+    pkgs.cargo-generate
+    pkgs.cargo-insta
+    pkgs.cargo-make
+    pkgs.deno
+    pkgs.dprint
+    pkgs.fnm
+    pkgs.git
+    pkgs.ripgrep
+    pkgs.rust-analyzer
+    pkgs.rustup
+    pkgs.trunk
+  ];
+
+  difftastic.enable = true;
+  devcontainer.enable = true;
+
+
+  # Scripts
+
+  scripts."build:all".exec = ''
+    cargo build
+  '';
+  scripts."fix:all".exec = ''
+    fix:format
+    fix:clippy
+  '';
+  scripts."fix:format".exec = ''
+    dprint fmt
+  '';
+  scripts."fix:clippy".exec = ''
+    cargo clippy --fix --allow-dirty --allow-staged
+  '';
+  scripts."lint:all".exec = ''
+    lint:format
+    lint:clippy
+  '';
+  scripts."lint:format".exec = ''
+    dprint check
+  '';
+  scripts."lint:clippy".exec = ''
+    cargo clippy
+  '';
+  scripts."test:snapshot".exec = ''
+    cargo insta accept
+  '';
+  scripts."test:all".exec = ''
+    cargo test
+  '';
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# leptos_aria
+
+> A port of the `react-aria` ecosystem for the leptos framework.
+
+## Motivation
+
+`leptos` is amazing and still in it's infancy and needs to better support accessibility.
+
+## Contributing
+
+[`devenv`](https://devenv.sh/) is used to provide a reproducible development environment for this
+project. Follow the [getting started instructions](https://devenv.sh/getting-started/).
+
+To automatically load the environment you should
+[install direnv](https://devenv.sh/automatic-shell-activation/) and then load the `direnv`.
+
+```bash
+# The security mechanism didn't allow to load the `.envrc`. 
+# Since we trust it, let's allow it execution.
+direnv allow .
+```
+
+At this point you should see the `nix` commands available in your terminal.
